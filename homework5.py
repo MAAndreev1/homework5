403234a9a59027359caedd8bbd249f279d463201
MAAndreev1 <167123315+MAAndreev1@users.noreply.github.com>
2024-04-30 19:25:32 +0500

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,11 @@
+my_list = ["яблоко", "банан", "персик", "груша", "виноград", "вишня"]
+print(my_list)
+print(my_list[0], my_list[-1])
+print(my_list[2:5])
+my_list[2] = 'СОЛНЦЕ'
+print(my_list)
+my_dict = {'лето': 'summer', 'яблоко': 'apple', 'телефон': 'phone'}
+print(my_dict)
+print(my_dict['лето'])
+my_dict.update({'лето': 'not fish','строка': 'string'})
+print(my_dict)
